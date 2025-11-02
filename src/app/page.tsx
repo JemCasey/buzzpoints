d27a7a35c2a1e1006653953317a90bee765bbb36
ani-per <amp7226@psu.edu>
2025-11-02 16:14:28 -0500

    Improve homepage blurb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,15 @@ export default function Home() {
   return (
     <Layout>
       <div>
-        Welcome to Buzzpoints! Click <Link href="/tournament" className="underline">here to view stats by tournament</Link> or <Link href="/set" className="underline">here to view stats by question set</Link>.
+        Welcome to Buzzpoints! Use the headers above to <Link href="/set" className="underline">here to view stats by question set</Link> or <Link href="/tournament" className="underline">here to view stats by tournament</Link>.
+      </div>
+      <br></br>
+      <div>
+        This website was created as a Vercel app from open-source code written by members of the quizbowl community.<br></br>The code processes QBJ data assembled using <Link href="https://github.com/alopezlago/MODAQ/" className="underline">MODAQ</Link> to read quizbowl packets.
+      </div>
+      <br></br>
+      <div>
+        To create your own buzzpoints site from QBJ data, read the directions in the <Link href="https://github.com/JemCasey/buzzpoints-migrator" className="underline">buzzpoints-migrator</Link> and <Link href="https://github.com/ani-per/buzzpoints" className="underline">buzzpoints</Link> GitHub repositories.
       </div>
     </Layout>
   );
